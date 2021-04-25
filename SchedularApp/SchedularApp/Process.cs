d085774825e7200436fb6fd2023a6187fa402146
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-04-25 16:03:29 +0200

    Non Preemptive Priority

---
+++ SchedularApp/SchedularApp/Process.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace SchedularApp
+{
+    class Process
+    {
+        private double arrival_time;
+        private double departure_time;
+        private double execution_time;
+        private double burst_time;
+        private double priority;
+        private double turnaround_time;
+        private string status;
+        private double remainingTime;
+
+        public double Arrival
+        {
+            set { arrival_time = value; }
+            get { return arrival_time; }
+        }
+        public double Burst
+        {
+            set { burst_time = value; }
+            get { return burst_time; }
+        }
+        public double Priority
+        {
+            set { priority = value; }
+            get { return priority; }
+        }
+        public double TurnAround
+        {
+            set { turnaround_time = value; }
+            get { return turnaround_time; }
+        }
+        public double Execution
+        {
+            set { execution_time = value; }
+            get { return execution_time; }
+        }
+        public double Departure
+        {
+            set { departure_time = value; }
+            get { return departure_time; }
+        }
+        public string Status
+        {
+            set { status = value; }
+            get { return status; }
+        }
+        public double Remaining
+        {
+            set { remainingTime = value; }
+            get { return remainingTime; }
+        }
+        public double WaitingTime()
+        {
+            return (turnaround_time - burst_time);
+        }
+        public Process()
+        {
+        }
+        public Process(double arrival, double burst)
+        {
+            arrival_time = arrival;
+            burst_time = burst;
+        }
+        public Process(double arrival, double burst, double p)
+        {
+            arrival_time = arrival;
+            burst_time = burst;
+            priority = p;
+            status = "active";
+            remainingTime = burst;
+        }
+    }
+}
